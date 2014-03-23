ba3fe201dadd10794f408193896ff5da904fea76
nitsood <nitsood@tamu.edu>
2014-03-23 15:55:05 -0500

    adding push_thread impl and pop_thread signature

--- MP4/Scheduler.C
+++ MP4/Scheduler.C
@@ -2,21 +2,33 @@
 
 Scheduler::Scheduler()
 {
+  num_threads = 0;
 }
 
 virtual void Scheduler::yield()
 {
 }
 
-virtual void Scheduler::resume(Thread *_thread)
+virtual void Scheduler::resume(Thread* _thread)
 {
+  push_thread(_thread);
 }
 
-
-virtual void Scheduler::add(Thread *_thread)
+virtual void Scheduler::add(Thread* _thread)
 {
+  push_thread(_thread);
 }
 
 virtual void Scheduler::terminate(Thread *_thread)
+{
+}
+
+void Scheduler::push_thread(Thread* th)
+{
+  ready_q[num_threads] = th;
+  num_threads++;
+}
+
+Thread* pop_thread();
 {
 }
